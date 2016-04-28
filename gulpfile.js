646fbc486beb78be752b46a1eaf91cfd5d9da44e
Zakaria Mahmud <zaka.pong@yahoo.com>
2016-04-28 10:04:01 +0200

    Revert "Revert "1st Push""
    This reverts commit 53db40d6ca20c483646fea7a82f1f68aef5450ff.

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+/**
+ * Created by zma on 2016-04-26.
+ */
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+var jsFiles = ['*js', 'src/**/*.js'];
+var nodemon = require('gulp-nodemon');
+
+gulp.task('style', function () {
+    return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+
+
+});
+
+gulp.task('inject', function () {
+
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: flase});
+
+    var injectOptions = {ignorePath: '/public'};
+
+    var options = {
+
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    }
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptons))
+        .pipe(gulp.dest('./src/views'));
+
+});
+
+gulp.task('server', ['style', 'inject'], function () {
+
+    var options = {
+
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 8989
+        },
+        watch: jsFiles
+
+
+    }
+
+
+    return nodemon(options)
+        .on('restart', function (ev) {
+
+            console.log('Restarting....')
+        })
+
+})
