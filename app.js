3b4159cd1892a29c4d5da53d8907f12f0bb03677
Zakaria Mahmud <zaka.pong@yahoo.com>
2016-04-28 15:24:51 +0200

    last

--- app.js
+++ app.js
@@ -1,81 +1,53 @@
-/**
- * Created by zma on 2016-04-25.
- */
-var express= require('express');
-
-var app= express();
-var sql = require('mssql');
-
-var config = {
-    user: '...',
-    password: '...',
-    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
-    database: '...',
-
-    options: {
-        encrypt: true // Use this if you're on Windows Azure
-    }
-};
-
-
-sql.connect(config).then(function(err) {
-    // Query
-
-    console.log(err);
-        // ... error checks
-    });
+var express = require('express');
+var bodyParser = require('body-parser');
 
 
 
+var app = express();
 
-
-
-var port= process.env.PORT || 8989;
-var nav= [{
-    Link:'/Books',
+var port = process.env.PORT || 8989;
+var nav = [{
+    Link: '/Books',
     Text: 'Book'
-},{
-    Link:'/Authors',
-    Text: 'Author'}];
+}, {
+    Link: '/Authors',
+    Text: 'Author'
+}];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 
 
-var bookRouter= require('./src/routes/bookRoutes')(nav);
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
-//app.set(express.static('public'));
+
+
+
 app.set('views', './src/views');
-//var handlebars= require('express-handlebars');
-//app.engine('.hbs', handlebars({extname: '.hbs'}));
 
-
-//app.set('view engine', '.hbs');
 app.set('view engine', 'ejs');
 
-
-
 app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
-app.get('/', function(req, res){
 
-   res.render('index', {
-       title:'hello from the samsu', nav: [{
-       Link:'/Books',
-     Text: 'Books'
-   },{
-       Link:'/Authors',
-    Text: 'Authors'}]
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
+//app.get('/books', function (req, res) {
+  //  res.send('Hello Books');
+//});
 
-});
-
-app.get('/books', function(req, res){
-
-    res.send('Hello Books');
-
-});
-
-app.listen(port, function(err){
-
-    console.log('running server on port'+ port);
-
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
 });
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,118 +1,63 @@
-var express= require('express');
-var bookRouter= express.Router();
-var sql= require('mssql');
+var express = require('express');
+var bookRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
-var router= function(nav){
+var router = function (nav) {
 
-    var books = [
-        {
-            title: 'War and Peace',
-            genre: 'Historical Fiction',
-            author: 'Lev Nikolayevich Tolstoy',
-            read: false
-        },
-        {
-            title: 'Les Misérables',
-            genre: 'Historical Fiction',
-            author: 'Victor Hugo',
-            read: false
-        },
-        {
-            title: 'The Time Machine',
-            genre: 'Science Fiction',
-            author: 'H. G. Wells',
-            read: false
-        },
-        {
-            title: 'A Journey into the Center of the Earth',
-            genre: 'Science Fiction',
-            author: 'Jules Verne',
-            read: false
-        },
-        {
-            title: 'The Dark World',
-            genre: 'Fantasy',
-            author: 'Henry Kuttner',
-            read: false
-        },
-        {
-            title: 'The Wind in the Willows',
-            genre: 'Fantasy',
-            author: 'Kenneth Grahame',
-            read: false
-        },
-        {
-            title: 'Life On The Mississippi',
-            genre: 'History',
-            author: 'Mark Twain',
-            read: false
-        },
-        {
-            title: 'Childhood',
-            genre: 'Biography',
-            author: 'Lev Nikolayevich Tolstoy',
-            read: false
+    bookRouter.use(function (req, res, next) {
+        if (!req.user) {
+            res.redirect('/');
         }
-    ];
-
-
+        next();
+    });
     bookRouter.route('/')
-        .all(function(req, res, next){
+        .get(function (req, res) {
+            var url =
+                'mongodb://localhost:27017/nodejs-express-web-apps';
 
-            var request= new sql.Request();
-            request.query('select * from books',
-                function(err, recordset){
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
 
-                    res.render('bookListView',{
-                        title:'Books', nav: nav,
-
-                        books: recordset
-                    });
-                    //      console.log(recordset);
-                });
+                collection.find({}).toArray(
+                    function (err, results) {
+                        res.render('bookListView', {
+                            title: 'Books',
+                            nav: nav,
+                            books: results
+                        });
+                    }
+                );
+            });
 
         });
+
     bookRouter.route('/:id')
-        .get(function(req, res){
+        .get(function (req, res) {
+            var id = new objectId(req.params.id);
+            var url =
+                'mongodb://localhost:27017/nodejs-express-web-apps';
 
-            var id= req.params.id;
-            var ps= new sql.PreparedStatement();
-            ps.input('id', sql.Int)
-            ps.prepare('select * from books where id=@id',
-                function(err){
-                    ps.execute({id:req.params.id},
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
 
-                        function(err, recordset) {
-                            if(recordset.length==0){
-                                res.status(404).send('Not found');
-                            }else{
-                                req.book= recordset[0];
-                                next();
-
-                            }
-
+                collection.findOne({
+                        _id: id
+                    },
+                    function (err, results) {
+                        res.render('bookView', {
+                            title: 'Books',
+                            nav: nav,
+                            book: results
                         });
 
-                });
+                    }
+                );
 
-        })
-        .get(function(req, res){
-            res.render('bookView',{
-
-                title: 'Books',
-                nav: nav,
-                book: req.book
-            })
-
-
-
-            // res.send('Hello single Books');
+            });
 
         });
 
     return bookRouter;
-}
-
-
-
-module.exports=router;
+};
+module.exports = router;
